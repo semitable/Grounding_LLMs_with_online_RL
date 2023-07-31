4d3fb0643e510c07f7320607100124aeb9677a83
Filippos Christianos <f.christianos@ed.ac.uk>
2023-07-31 18:19:09 +0100

    Fixes babyai packages with find_packages

--- babyai/setup.py
+++ babyai/setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='babyai',
     version='0.1.0',
     license='BSD 3-clause',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
+    packages=find_packages(),
     # packages=['babyai', 'babyai.rl', 'babyai.levels', 'babyai.utils'],
     install_requires=[
         'gym>=0.9.6,<0.26.2',
