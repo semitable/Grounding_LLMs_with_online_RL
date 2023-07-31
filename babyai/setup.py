cf9c90cc12c6b00e665e3c2456c82dcaffe38cb0
Filippos Christianos <f.christianos@ed.ac.uk>
2023-07-31 18:21:15 +0100

    Adds matplotlib dependency

--- babyai/setup.py
+++ babyai/setup.py
@@ -12,6 +12,7 @@ setup(
         'numpy>=1.17.0',
         "torch>=0.4.1",
         'blosc>=1.5.1',
+        "matplotlib>=3.7",
         "tqdm==4.65",
         # 'gym_minigrid @ https://github.com/maximecb/gym-minigrid/archive/master.zip'
     ],
